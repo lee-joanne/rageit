00d24dca21ae449c4248fe58dfb48dd94149952e
Joanne <lee.joanne54@gmail.com>
2022-07-04 19:53:51 +0000

    Create Post class in models.py and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import datetime
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Post model class
+    """
+    title = models.CharField(max_length=250, null=False, blank=False, unique=True)
+    slug = models.SlugField(null=True, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_author")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField(max_length=2500)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(max_length=150)
+    created_on = models.DateTimeField(default=datetime.datetime.now)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """String for representing Post object in admin"""
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_title", kwargs={"slug": self.slug})
+
+class Comment(models.Model):
+    """
+    Comment model class
+    """
+    pass
