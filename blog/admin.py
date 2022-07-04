306f075d398db953b77f95baa0b2aa2a0d2aaf40
Joanne <lee.joanne54@gmail.com>
2022-07-04 20:48:56 +0000

    Create PostAdmin class in admin.py file

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django import forms
 from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_slug_fields = {'slug': ('title',)}
+    prepopulated_excerpt_fields = {'excerpt': ('content',)}
--- blog/models.py
+++ blog/models.py
@@ -11,11 +11,11 @@ class Post(models.Model):
     slug = models.SlugField(null=True, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_author")
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField(max_length=2500)
+    content = CKEditor5Field('Text', config_name='extends')
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(max_length=150)
     created_on = models.DateTimeField(default=datetime.datetime.now)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    #likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -23,9 +23,6 @@ class Post(models.Model):
     def __str__(self):
         """String for representing Post object in admin"""
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("post_title", kwargs={"slug": self.slug})
 
 class Comment(models.Model):
     """
