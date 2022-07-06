f3feef9cd894674541a2d04eae639eb1f9632ba3
Joanne <lee.joanne54@gmail.com>
2022-07-06 19:07:40 +0000

    Add index.html url and create view function to begin template for front-end

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Post, Comment
 
+
+def blogmain_view(request):
+    return render(request, "index.html")
--- rageit/urls.py
+++ rageit/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import blogmain_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', blogmain_view, name="blogmain_view")
 ]
