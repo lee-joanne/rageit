69af6e490e3eac2e22f2fd7cb0bc6ba3b1a20db1
Joanne <lee.joanne54@gmail.com>
2022-07-11 20:50:09 +0000

    Add Bootstrap layouts to index.html, show title, excerpt, and number of likes in index.html from models.py

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.views.generic import ListView, View
 from .models import Post
 
 
-class PostView(generic.ListView):
+class PostView(ListView):
     '''
     Class-based view for homepage to show lists of posts
     '''
     model = Post
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 8
-
-
-
