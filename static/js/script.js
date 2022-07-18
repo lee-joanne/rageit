de316b4ce0e7f10201c5ba5a873259bc1e1c7b7d
Joanne <lee.joanne54@gmail.com>
2022-07-18 20:38:46 +0000

    Add javascript functions to show messages in sign up html page

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+let signUpMessage = document.getElementById("signup-message")
+let signUpHover = document.getElementById("signup-hover")
+
+signUpHover.addEventListener("mouseover", showMessage);
+signUpHover.addEventListener("mouseout", hideMessage);
+
+function showMessage() {
+    /*
+    Function to preview sign up message in Sign Up html page when user hovers mouse over arrow
+    */
+    signUpMessage.classList.remove("hide");
+}
+
+function hideMessage() {
+    /*
+    Function to hide sign up message in Sign Up html page when user removes mouse over arrow
+    */
+    signUpMessage.classList.add("hide");
+}
