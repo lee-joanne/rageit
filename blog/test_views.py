9bf35b79c22a0decf2bec0fd38f759ae011c7737
Joanne <lee.joanne54@gmail.com>
2023-09-11 19:11:33 +0000

    Test post likes exist liked true approved by coverage

--- blog/test_views.py
+++ blog/test_views.py
@@ -4,6 +4,8 @@ from blog.models import Post, Comment
 from django.utils import timezone
 from django.urls import reverse
 from .forms import CommentForm
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 
 class Test_Views(TestCase):
@@ -39,3 +41,22 @@ class Test_Views(TestCase):
         self.assertEqual(response.context['slug'], post.slug)
         self.assertTrue(post.likes.filter(id=user.id).exists())
         self.assertFalse(response.context['is_revised'])
+
+    def test_post_likes_exist_liked_true(self):
+        post = Post.objects.get(id=1)
+        user = User.objects.get(username='test_user2')
+        self.client.login(username='test_user2', password='123456')  # Log in the user
+        post.likes.add(user)
+        post.save()
+        response = self.client.get(reverse('post_detailed_view', args=[post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['post'], post)
+        self.assertEqual(response.context['slug'], post.slug)
+        self.assertTrue(response.context['liked'])
+        
+    def test_get_object_or_404(self):
+        post = get_object_or_404(Post, slug='post-title')
+        with self.assertRaises(Http404):
+            post2 = get_object_or_404(Post, slug='non-existent')
+        response = self.client.get(reverse('post_detailed_view', args=[post.slug]))
+        self.assertEqual(response.status_code, 200)
